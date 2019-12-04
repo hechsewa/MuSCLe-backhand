daf8d0f42cbda35b26e10023d5330445e1645cbd
Ewa Hechsman <ewa.hechsman@o2.pl>
2019-12-04 15:45:50 +0100

    comand

--- __init__.py
+++ __init__.py
@@ -1,10 +1,9 @@
 import os
-
+import click
 from flask import Flask
+from flask.cli import with_appcontext
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
-from commands import create_tables
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 CORS(app)
@@ -14,4 +13,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://3wvzLcjQzD:RArEPgAhKS@remotemysql.com:3306/3wvzLcjQzD'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+
+
+@click.command(name='create_tables')
+@with_appcontext
+def create_tables():
+    db.create_all()
+
+
 app.cli.add_command(create_tables)
--- commands.py
+++
@@ -1,9 +0,0 @@
-from __init__ import db
-import click
-from flask.cli import with_appcontext
-
-
-@click.command(name='create_tables')
-@with_appcontext
-def create_tables():
-    db.create_all()
