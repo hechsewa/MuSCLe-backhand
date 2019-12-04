e5d3b2d5bd8f8973852776b39b71fccc7d049cbf
Ewa Hechsman <ewa.hechsman@o2.pl>
2019-12-04 15:35:30 +0100

    ..

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
+from commands import create_tables
+
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 CORS(app)
 
@@ -12,3 +14,4 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://3wvzLcjQzD:RArEPgAhKS@remotemysql.com:3306/3wvzLcjQzD'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+app.cli.add_command(create_tables)
